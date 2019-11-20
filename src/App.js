7bf6f4608d23746311d42298a40095b9aad47fd7
sethukumar0001 <boylooksrock@gmail.com>
2019-11-20 15:49:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import ExcelPage from "./components/excelPage";
+
+function App() {
+  return (
+    <>
+      <ExcelPage />
+    </>
+  );
+}
+
+export default App;
